962dbcc5e3a8e209400b58cd19bb9bbdd3c89f8b
SuperninjaX2 <80552583+SuperninjaX2@users.noreply.github.com>
2024-05-31 18:46:10 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
+function fade() {
+  const btn = document.querySelector(".btn");
+  const cir = document.createElement("div");
 
+  btn.addEventListener("click", () => {
+    document.body.appendChild(cir);
+    cir.classList.add("cir");
+
+    gsap.to("body", {
+      background: "#fff",
+      duration: 3
+    });
+
+    gsap.to(".cir", {
+      scaleX: window.innerWidth / btn.getBoundingClientRect().width,
+      scaleY: window.innerHeight / btn.getBoundingClientRect().height,
+      width: "100vw",
+      height: "100vh",
+      background: "#fff",
+      borderRadius: "50%",
+      duration: 6
+    });
+  });
+}
